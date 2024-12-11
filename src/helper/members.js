2ccfb0a4e2db260e34ad34348f460a30dcc555d1
gabs <gabriellenatashaa@gmail.com>
2024-12-11 18:37:39 +0800

    Prepare for demo

--- src/helper/members.js
+++ src/helper/members.js
@@ -1,23 +1,43 @@
 const members = [
   {
-    name: "CJ",
-    username: "callumjeanpierre",
-    img: "/CJ.png",
-  },
-  {
-    name: "Darn",
-    username: "darenwithoner",
-    img: "/Darn.png",
+    name: "Demo",
+    username: "demouser",
+    img: "/demouser.png",
   },
   {
     name: "U1",
-    username: "userone",
-    img: "/Darn.png",
+    username: "dummyuser1",
+    img: "/dummy1.png",
   },
   {
     name: "U2",
-    username: "usertwo",
-    img: "/Darn.png",
+    username: "dummyuser2",
+    img: "/dummy2.png",
+  },
+  {
+    name: "U3",
+    username: "dummyuser3",
+    img: "/dummy3.png",
+  },
+  {
+    name: "U4",
+    username: "dummyuser4",
+    img: "/dummy4.png",
+  },
+  {
+    name: "U5",
+    username: "dummyuser5",
+    img: "/dummy5.png",
+  },
+  {
+    name: "U6",
+    username: "dummyuser6",
+    img: "/dummy6.png",
+  },
+  {
+    name: "U7",
+    username: "dummyuser7",
+    img: "/dummy7.png",
   },
 ];
 
