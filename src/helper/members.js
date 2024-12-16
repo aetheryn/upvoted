3f9551c5a2133071970841cbb85aff2e000c7fb5
gabs <gabriellenatashaa@gmail.com>
2024-12-16 22:50:31 +0800

    Update members.js (without profile picture)

--- src/helper/members.js
+++ src/helper/members.js
@@ -1,43 +1,103 @@
 const members = [
   {
-    name: "Demo",
-    username: "demouser",
+    name: "¿?",
+    username: "rui",
     img: "/demouser.png",
   },
   {
-    name: "U1",
-    username: "dummyuser1",
+    name: "Brian",
+    username: "almightybri",
     img: "/dummy1.png",
   },
   {
-    name: "U2",
-    username: "dummyuser2",
+    name: "Charlie",
+    username: "charliesangels",
     img: "/dummy2.png",
   },
   {
-    name: "U3",
-    username: "dummyuser3",
+    name: "Elle",
+    username: "mareesapage",
     img: "/dummy3.png",
   },
   {
-    name: "U4",
-    username: "dummyuser4",
+    name: "Eulalie A. B.",
+    username: "eulaventorous",
     img: "/dummy4.png",
   },
   {
-    name: "U5",
-    username: "dummyuser5",
+    name: "Evangeline",
+    username: "onceuponEvangeline",
     img: "/dummy5.png",
   },
   {
-    name: "U6",
-    username: "dummyuser6",
+    name: "Hashiiෆ",
+    username: "HashiiPwetal",
     img: "/dummy6.png",
   },
   {
-    name: "U7",
-    username: "dummyuser7",
+    name: "Katherine",
+    username: "kathmeifyoucan",
     img: "/dummy7.png",
+  },
+  {
+    name: "Kathryn",
+    username: "kathrysnne",
+    img: "/demouser.png",
+  },
+  {
+    name: "Kousuke",
+    username: "koufujiwara",
+    img: "/dummy1.png",
+  },
+  {
+    name: "Lidia, P.",
+    username: "lidiaverse",
+    img: "/dummy2.png",
+  },
+  {
+    name: "Luca Rivera",
+    username: "xxrivera",
+    img: "/dummy3.png",
+  },
+  {
+    name: "Lucian Bara",
+    username: "callmebara",
+    img: "/dummy4.png",
+  },
+  {
+    name: "Miro",
+    username: "oftakeshi",
+    img: "/dummy5.png",
+  },
+  {
+    name: "Nero",
+    username: "neronfleu",
+    img: "/dummy6.png",
+  },
+  {
+    name: "Shanette",
+    username: "ShanetteB",
+    img: "/dummy7.png",
+  },
+  {
+    name: "Shenina.",
+    username: "shenshine",
+    img: "/demouser.png",
+  },
+  {
+    name: "Sho",
+    username: "onishoma",
+    img: "/dummy1.png",
+  },
+  {
+    name: "Xane",
+    username: "thexaneone",
+    img: "/dummy2.png",
+  },
+  {
+    name: "Yvonne",
+    username: "everyvonne",
+    img: "/dummy3.png",
   },
 ];
 
